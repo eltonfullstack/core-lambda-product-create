3464e08ce518231a7dad8b2334f77f0c76074593
Elton Silva <eltonfullstack@gmail.com>
2026-04-20 20:20:58 -0300

    Substituir o any por types

--- app/src/modules/product/infra/lambda/createProduct.ts
+++ app/src/modules/product/infra/lambda/createProduct.ts
@@ -1,8 +1,10 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import { createProductController } from "../../../../modules/product/interface/http/controllers/productController"
 import { handleHttpError } from "../../../../modules/product/interface/http/errors/handleHttpError"
 
-export const handler = async (event: any) => {
-
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   try {
     return await createProductController(event)
   } catch (error) {
