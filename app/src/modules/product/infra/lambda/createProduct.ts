f5e8cdd4668ed7efbb4d1033394ac28ad5648b84
Elton Silva <eltonfullstack@gmail.com>
2026-04-21 09:54:53 -0300

    Ajuste handler

--- app/src/modules/product/infra/lambda/createProduct.ts
+++ app/src/modules/product/infra/lambda/createProduct.ts
@@ -1,11 +1,16 @@
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import { createProductController } from "../../../../modules/product/interface/http/controllers/productController"
 import { handleHttpError } from "../../../../modules/product/interface/http/errors/handleHttpError"
+import logger from '@shared/logger/logger'
 
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   try {
+
+    logger.info('Creating product handler')
+    logger.info('event', event as any)
+    
     return await createProductController(event)
   } catch (error) {
     return handleHttpError(error)
